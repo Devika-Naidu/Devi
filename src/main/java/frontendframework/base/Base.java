da606f10e2bf0a1ebc1b68c94fcd3764267362bf
Devika Naidu <devikanaidu8791@gmail.com>
2024-09-06 14:55:22 +0200

    created Excel data,static class,extend reports class

--- src/main/java/frontendframework/base/Base.java
+++ src/main/java/frontendframework/base/Base.java
@@ -1,5 +1,6 @@
 package frontendframework.base;
 
+import frontendframework.resource.staticResource.Static;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -12,36 +13,30 @@ import java.util.Properties;
 
 public class Base
 {
-    WebDriver driver;
-    Properties prop;
     public void loadProperties() throws IOException {
-        FileInputStream file=new FileInputStream("C:\\hybrid\\DeviFramework\\Frontend_Framework\\src\\main\\java\\frontendframework\\config\\Config.properties");
-       prop=new Properties();
-        prop.load(file);
+        FileInputStream file=new FileInputStream("C:\\hybrid\\DeviFramework\\Devi\\src\\main\\java\\frontendframework\\config\\Config.properties");
+      Static.prop=new Properties();
+        Static.prop.load(file);
     }
     public void openBrowser(String browser)
     {
         if(browser.equals("Chrome")||browser.equals("chrome"))
-            driver=new ChromeDriver();
+            Static.driver=new ChromeDriver();
        if(browser.equals("Firefox")||browser.equals("firefox"))
-        driver=new FirefoxDriver();
+        Static.driver=new FirefoxDriver();
        if(browser.equals("Edge")||browser.equals("edge"))
-           driver=new EdgeDriver();
+        Static.driver=new EdgeDriver();
 
     }
      public void launchBrowser() throws IOException, InterruptedException {
         loadProperties();
-        String br=prop.getProperty("Browser");
+        String br=Static.prop.getProperty("Browser");
          System.out.println(br);
         openBrowser(br);
-        driver.manage().window().maximize();
-       //driver.get("https://ui.cogmento.com/");
-
-       String url=prop.getProperty("Url");
-      // driver.get(prop.getProperty("url"));
-       driver.get(url);
-        Thread.sleep(2000);
-       // System.out.println(url);
+        Static.driver.manage().window().maximize();
+         String url=Static.prop.getProperty("Url");
+         Static.driver.get(url);
+        System.out.println(url);
      }
 
     public static void main(String[] args) throws IOException, InterruptedException {
--- src/main/java/frontendframework/resource/staticResource/Static.java
+++ src/main/java/frontendframework/resource/staticResource/Static.java
@@ -1,8 +1,13 @@
 package frontendframework.resource.staticResource;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
 public class Static
 {
-
+public static WebDriver driver;
+public static Properties prop;
 
 
 }
